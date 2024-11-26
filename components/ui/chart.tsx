6273c96474b52f73602ab4cae6141800d654f9d3
TuanDinh140194 <tnd@pdx.edu>
2024-11-26 14:28:25 -0800

    third  commit

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable tailwindcss/no-custom-classname */
 
 "use client"
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { FileType } from "@/types";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
